2e3abf09db25044490f0391f5b4845118f3a6aa8
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-18 09:39:39 -0700

    add user registration screen

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBU0opnzvAjxhNAJBIjKrogrw-Sus-NaM",
+  authDomain: "swimpnw.firebaseapp.com",
+  projectId: "swimpnw",
+  storageBucket: "swimpnw.appspot.com",
+  messagingSenderId: "346492400252",
+  appId: "1:346492400252:web:c4d03fc26f3578da684ff9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
